3d6b6abc75d42adcf830d89dbc2d96183a043dab
Jan Philipp Bohl <jpbohl@protonmail.com>
2024-01-08 11:00:25 +0100

    Ablation study ready

---
+++ AblateCNO.py
@@ -0,0 +1,110 @@
+import copy
+import json
+import os
+import sys
+import atexit
+
+import pandas as pd
+import torch
+from tqdm import tqdm
+
+import wandb
+import matplotlib.pyplot as plt
+
+from Problems.Straka import Straka
+from TrainUtils import Trainer
+
+if len(sys.argv) == 1:
+
+    cluster = False
+    mode = "disabled"
+
+    training_properties = {
+        "learning_rate": 0.001, 
+        "weight_decay": 1e-6,
+        "scheduler_step": 10,
+        "scheduler_gamma": 0.98,
+        "epochs": 2,
+        "batch_size": 16,
+        "exp": 1, #Do we use L1 or L2 errors? Default: L1
+        "training_samples": 4, #How many training samples?
+    }
+    
+    with open("cno_architecture.json", "r") as f:
+        model_architecture_ = json.loads(f.read().replace("\'", "\""))
+
+    ablation = pd.read_csv("Ablation.csv") 
+    counter = 22
+    
+    time = 300
+    dataloc = "/Users/jan/sempaper/StrakaData/"
+
+    # Save the models here:
+    folder = "TrainedModels/"
+        
+else:
+    
+    cluster = True
+    mode = "online"
+
+    # Do we use a script to run the code (for cluster):
+    folder = sys.argv[1] 
+
+    # Reading hyperparameters
+    with open(sys.argv[2], "r") as f:
+        training_properties = json.loads(f.read().replace("\'", "\""))
+    with open(sys.argv[3], "r") as f:
+        model_architecture_ = json.loads(f.read().replace("\'", "\""))
+    ablation = pd.read_csv(sys.argv[4])
+
+    # Determine problem to run and data location
+    time = int(sys.argv[5])
+    dataloc = sys.argv[6]
+
+    # Ablation study iteration 
+    counter = int(os.environ["SLURM_ARRAY_TASK_ID"])
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Update model architecture with ablation parameters
+ablation = dict(ablation.iloc[counter])
+for name, val in ablation.items():
+    model_architecture_[name] = val
+
+wandb.login()
+config = {"time" : time, **training_properties, **model_architecture_}
+run = wandb.init(
+    project = "StrakaCNO", 
+    mode = mode,
+    name = f"ablation-{counter}",
+    config=config)
+folder += run.name
+
+#-----------------------------------Train--------------------------------------
+
+# Get fixed training properties
+epochs = training_properties["epochs"]
+batch_size = training_properties["batch_size"]
+training_samples = training_properties["training_samples"]
+p = training_properties["exp"]
+s = model_architecture_["in_size"]
+
+if not os.path.isdir(folder):
+    print("Generated new folder")
+    os.mkdir(folder)
+
+example = Straka(model_architecture_, device, batch_size, training_samples, time=time, s=s, dataloc=dataloc, cluster=cluster)
+trainer = Trainer(training_properties, example, device)
+
+# Training loop
+for epoch in range(epochs):
+
+    train_mse = trainer.train_epoch()
+    val_loss = trainer.validate()
+
+    print(f"Epoch {epoch} // MSE {train_mse} // Relative Val Loss {val_loss}")
+        
+    wandb.log(({
+        "Train Loss" : train_mse,
+        "Relative L2 Test Error" : val_loss}), 
+        step=epoch)
---
+++ TrainUtils.py
@@ -0,0 +1,121 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Trainer():
+
+    def __init__(self, training_properties, example, device):
+        
+        # Loading training properties
+        learning_rate = training_properties["learning_rate"]
+        weight_decay = training_properties["weight_decay"]
+        scheduler_step = training_properties["scheduler_step"]
+        scheduler_gamma = training_properties["scheduler_gamma"]
+        
+        # Get model and dataset
+        self.model = example.model
+        n_params = self.model.print_size()
+        self.train_loader = example.train_loader #TRAIN LOADER
+        self.val_loader = example.val_loader #VALIDATION LOADER
+        
+        # Set up optimizer and scheduler
+        self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=scheduler_step, gamma=scheduler_gamma)
+        
+        # Loss Function
+        self.loss = torch.nn.MSELoss()
+        self.device = device
+
+    def train_epoch(self):
+        self.model.train()
+        train_mse = 0.0
+        running_relative_train_mse = 0.0
+        for step, (input_batch, output_batch) in enumerate(self.train_loader):
+            self.optimizer.zero_grad()
+            input_batch = input_batch.to(self.device)
+            output_batch = output_batch.to(self.device)
+
+            output_pred_batch = self.model(input_batch)
+
+            loss_f = self.loss(output_pred_batch, output_batch) / self.loss(torch.zeros_like(output_batch).to(self.device), output_batch)
+
+            loss_f.backward()
+            self.optimizer.step()
+            train_mse = train_mse * step / (step + 1) + loss_f.item() / (step + 1)
+
+        self.scheduler.step()
+
+        return train_mse
+
+    def validate(self):
+        
+        with torch.no_grad():
+            self.model.eval()
+            test_relative_l2 = 0.0
+            train_relative_l2 = 0.0
+            
+            for step, (input_batch, output_batch) in enumerate(self.val_loader):
+                
+                input_batch = input_batch.to(self.device)
+                output_batch = output_batch.to(self.device)
+                output_pred_batch = self.model(input_batch)
+                
+                loss_f = torch.mean(abs(output_pred_batch - output_batch)) / torch.mean(abs(output_batch)) * 100
+                test_relative_l2 += loss_f.item()
+            test_relative_l2 /= len(self.val_loader)
+
+            return test_relative_l2
+
+    def log_plots(self):
+        """
+        Plotting function called when training loop exits.
+        Plots the predictions on a batch of the test set and
+        uploads them to wandb.
+        """
+        # Get single test batch
+        input_batch, output_batch = next(iter(self.val_loader))
+        input_batch = input_batch.to(device)
+        output_batch = output_batch.to(device)
+
+        with torch.no_grad():
+            pred = self.model(input_batch)
+            diffs = output_batch - pred
+
+        # Logging initial conidition channel of inputs as well as outputs and
+        # differences between predictions and labels
+        input_img = input_batch[0, 2, :, :].detach().cpu().numpy().T
+        pred_img = pred[0, 0, :, :].cpu().numpy().T
+        label = output_batch[0, 0, :, :].detach().cpu().numpy().T
+        diff_img = diffs[0, 0, :, :].cpu().numpy().T
+
+        # Plotting intial condition
+        figi, ax = plt.subplots()
+
+        ic = ax.pcolormesh(input_img, cmap="Blues_r")
+        ax.set_title("Initial condition")
+        figi.colorbar(ic, ax=ax)
+
+
+        # Plotting predictions
+        figp, axes = plt.subplots(1, 2, sharey=True)
+
+        labels = axes[0].pcolormesh(label, cmap="Blues_r")
+        axes[0].set_title("Labels")
+        figp.colorbar(labels, ax=axes[0])
+
+        fno = axes[1].pcolormesh(pred_img, cmap="Blues_r")
+        axes[1].set_title("CNO")
+        figp.colorbar(fno, ax=axes[1])
+
+        # Plotting errors
+        fige, axes = plt.subplots(1, 2, sharey=True)
+
+        labels = axes[0].pcolormesh(label, cmap="Blues_r")
+        axes[0].set_title("Labels")
+        fige.colorbar(labels, ax=axes[0])
+
+        error_fno = axes[1].pcolormesh(diff_img, cmap="Blues_r")
+        axes[1].set_title("CNO Error")
+        fige.colorbar(error_fno, ax=axes[1])
+
+        return figi, figp, fige
